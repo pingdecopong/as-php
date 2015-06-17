2d493ffe01c0bd0ad3187c6ef873de0450095c74
fhirashima <fhirashima@arte-sys.jp>
2015-06-17 09:53:26 +0900

    php.iniパラメータ追加

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,11 @@
 
 default['php']['version'] = '5.4'
-default['php']['xdebug']['remote_enable'] = true
+default['php']['xdebug']['remote_enable'] = false
 default['php']['xdebug']['remote_host'] = '10.0.2.2'
 default['php']['xdebug']['remote_port'] = '9000'
+
+default['php']['max_execution_time'] = '30'
+default['php']['memory_limit'] = '128M'
+default['php']['post_max_size'] = '8M'
+default['php']['upload_max_filesize'] = '2M'
+default['php']['date.timezone'] = ''
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ source_url       'https://github.com/pingdecopong/as-php'
 license          'MIT'
 description      'Installs/Configures as-php'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
 
 depends 'yum-epel'
 depends 'yum-remi'
